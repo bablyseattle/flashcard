be0a3af3b0f8aa2d677219a316c0be62cf65cdff
Bably Kumari and Debbie Michel <bably@>
2017-08-08 14:51:09 -0700

    Add hide function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-  $(".clickable1").click(function() {
+  $("#js").click(function() {
+    $("#js").hide();
     $("#jsTerm").toggle();
-  });
+
+});
+  $("#jsTerm").click(function() {
+    $("#jsTerm").hide();
+    $("#js").toggle();
+
+});
 $(".clickable2").click(function() {
 $("#operatorTerm").toggle();
 });
 $(".clickable3").click(function() {
 $("#variablesTerm").toggle();
 });
+
 });
