f5d1e40b12197977127ff374612f93971b85dbca
Bably Kumari and Debbie Michel <debbie@>
2017-08-08 13:51:16 -0700

    Add class and ID to make definition appear and disappear.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#jsTerm").toggle();
+  });
+$(".clickable2").click(function() {
+$("#operatorTerm").toggle();
+});
+$(".clickable3").click(function() {
+$("#variablesTerm").toggle();
+});
+});
